1eeec9870a4e4d44037b59323184c91b945184f2
DruideFunky462 <93768876+DruideFunky462@users.noreply.github.com>
2022-06-23 04:56:21 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\piscineqnamaker-bot-95d3-Web-Deploy.pubxml -p:Password=7plnNb97oNdfX0v493YJXJRMJooBnb56BhLLvo3c6mjNKboDn1AaGk16AgPc
+
